d911e3d46f3a545a3f596854f5e2e619e00267c9
George Jimy <knivespeans@gmail.com>
2025-03-11 14:56:15 -0500

    avance movimiento Peon y entorno

--- Assets/Scripts/GrabPiece.cs
+++ Assets/Scripts/GrabPiece.cs
@@ -8,27 +8,34 @@ public class GrabPiece : SpatialNetworkBehaviour
     private GameObject piece;
     private bool isGrab;
 
+    [Header("GroundMaterials")]
+    public Material defaultMaterial;
+    public Material highlightMaterial;
+
+    public ArrayMap map;
     private void Update()
     {
         if (isGrab)
             piece.transform.position = SpatialBridge.actorService.localActor.avatar.position;
-            
+
 
     }
     public void Grab(GameObject piece)
     {
-        if(!isGrab)
+        if (!isGrab)
         {
             isGrab = true;
             this.piece = piece;
             GiveControl(this.piece);
+            CalculatePieceMove();
         }
         else
         {
             isGrab = false;
             this.piece = null;
+            ResetHighlights();
         }
-        
+
     }
     public void GiveControl(GameObject pieceNetwork)
     {
@@ -38,5 +45,64 @@ public class GrabPiece : SpatialNetworkBehaviour
             obj.RequestOwnership();
         }
 
+    }
+    void ResetHighlights()
+    {
+        foreach (var m in map.maps)
+        {
+            MeshRenderer mr = m.ground.GetComponent<MeshRenderer>();
+            if (mr != null)
+                mr.material = defaultMaterial;
+        }
+    }
+    void HighlightGround(string letra, int numero)
+    {
+        foreach (var m in map.maps)
+        {
+            if (m.name == letra && m.number == numero)
+            {
+                MeshRenderer mr = m.ground.GetComponent<MeshRenderer>();
+                if (mr != null)
+                    mr.material = highlightMaterial;
+            }
+        }
+    }
+
+    public void CalculatePieceMove()
+    {
+        ResetHighlights();
+
+        PieceType pieceType = piece.GetComponent<PieceType>();
+        int direction = (pieceType.color == PieceType.PieceColor.Blanco) ? 1 : -1;
+
+        switch (piece.GetComponent<PieceType>().type)
+        {
+            case PieceType.Type.Peon:
+
+                PiecePositionDetector piecePositionDetector = piece.GetComponent<PiecePositionDetector>();
+
+                int nextNumber = piecePositionDetector.currentNumber + direction;
+                HighlightGround(piecePositionDetector.currentLetter, nextNumber);
+
+                if ((pieceType.color == PieceType.PieceColor.Blanco && piecePositionDetector.currentNumber == 2) ||
+                (pieceType.color == PieceType.PieceColor.Negro && piecePositionDetector.currentNumber == 7))
+                {
+                    HighlightGround(piecePositionDetector.currentLetter, piecePositionDetector.currentNumber + (2 * direction));
+                }
+                break;
+
+            case PieceType.Type.Torre:
+                break;
+            case PieceType.Type.Caballo:
+                break;
+            case PieceType.Type.Alfil:
+                break;
+            case PieceType.Type.Reina:
+                break;
+            case PieceType.Type.Rey:
+                break;
+            default:
+                break;
+        }
     }
 }
